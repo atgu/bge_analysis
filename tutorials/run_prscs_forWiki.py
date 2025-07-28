59426ad59939e72806b249b8d161c9d4a688213e
Toni Boltz <toniaboltz@gmail.com>
2025-07-28 16:32:06 -0400

    Update run_prscs_forWiki.py to Uganda cohort

--- tutorials/run_prscs_forWiki.py
+++ tutorials/run_prscs_forWiki.py
@@ -5,18 +5,18 @@ N=170114
 
 #the LD reference was downloaded from the PRS-CS github repo, see here: https://github.com/getian107/PRScs
 #specifically the AFR 1KG reference panel: ldblk_1kg_afr.tar.gz
-refdir="gs://neurogap-bge-imputed-regional/prscs/ldblk_1kg_afr"
+refdir="gs://neurogap-bge-imputed-regional/toni/prscs/ldblk_1kg_afr"
 
 for chrom in range(1, 23):
 do
-    bfile_path = "gs://neurogap-bge-imputed-regional/forPRSWiki/AAU_prs_chr"${chrom}
+    bfile_path = "gs://neurogap-bge-imputed-regional/toni/forPRSWiki/Uganda_prs_chr"${chrom}
 
     python3 PRScs.py \
     --ref_dir=${ref_dir} \
     --bim_prefix=${bfile_path} \
     --sst_file=${sst} \
     --n_gwas=${N} \
-    --out_dir=tmp_prscs_output/AAU_PRSCS \
+    --out_dir=tmp_prscs_output/Uganda_PRSCS \
     --chrom=${chrom}
 
 done
