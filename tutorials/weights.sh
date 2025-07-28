b323303cd8710e948db0b8c4354ce0c6229582e9
Toni Boltz <toniaboltz@gmail.com>
2025-07-28 16:32:35 -0400

    Update weights.sh to Uganda cohort

--- tutorials/weights.sh
+++ tutorials/weights.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #concatenate all per-chromsome SNP weights to one file 
-cat AAU_PRSCS_pst_eff_a1_b0.5_phiauto_chr* > AAU_weights.txt
+cat Uganda_PRSCS_pst_eff_a1_b0.5_phiauto_chr* > Uganda_weights.txt
 
 #use plink to get scores of the AAU cohort
-plink2 --bfile AAU_prs_qc2 --score AAU_weights.txt cols=+scoresums ignore-dup-ids 2 4 6  --out AAU_scores
+plink2 --bfile Uganda_prs_qc2 --score Uganda_weights.txt cols=+scoresums ignore-dup-ids 2 4 6  --out Uganda_scores
 
